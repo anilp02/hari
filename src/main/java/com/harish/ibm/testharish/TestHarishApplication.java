b63a6d2e0a1121ca5dd0a73293181f174e86531c
anilp02 <anilkumarp31@gmail.com>
2018-06-08 07:14:16 -0400

    initial

---
+++ src/main/java/com/harish/ibm/testharish/TestHarishApplication.java
@@ -0,0 +1,12 @@
+package com.harish.ibm.testharish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHarishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHarishApplication.class, args);
+	}
+}
